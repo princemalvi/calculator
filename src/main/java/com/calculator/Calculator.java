eeddbf6d1ceee5ea4ee2f9efa03d0ca594d96f46
princemalvi097 <princemalvi097@gmail.com>
2021-08-20 17:44:20 +0530

    Initial Commit

---
+++ src/main/java/com/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package com.calculator;
+
+public class Calculator 
+{
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/calculator/CalculatorTest.java
@@ -0,0 +1,16 @@
+package com.calculator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class CalculatorTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+
+}
